b8143b9b824135d6aa3bea9fe7a8b22cc4cfda61
LudmilaNepom <ludmilanepom80@gmail.com>
2017-05-11 13:23:17 +0300

    commit 11.09.17-2

---
+++ controllers/src/main/java/ua/mk/nepomnyachshaya/controllers/autor/AuthorController.java
@@ -0,0 +1,19 @@
+package ua.mk.nepomnyachshaya.controllers.autor;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+/**
+ *
+ */
+@Controller
+@RequestMapping(value = "/author")
+public class AuthorController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView getCategories() {
+        return new ModelAndView("author/index");
+    }
+
+
+}
